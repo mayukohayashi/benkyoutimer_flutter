9099bfe9f0c212c53fc675362a8d1a5088e1e680
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2021-04-12 15:17:42 +0900

    add: card

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
 }
 
-abstract class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: "rent",
+      amount: 100.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: "noodle",
+      amount: 22.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: "Wage",
+      amount: 32.99,
+      date: DateTime.now(),
+    )
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -35,12 +58,48 @@ abstract class MyHomePage extends StatefulWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text("List of TX"),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20;
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(
+                          tx.date.toString(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
   }
 }
