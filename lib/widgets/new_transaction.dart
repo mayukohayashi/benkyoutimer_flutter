80bbd51fc8beab6a6ddac444756a91d50281c2de
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2021-04-15 19:07:08 +0900

    add: Amount部に出方調整

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,18 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,25 +32,18 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "Title"),
               controller: titleController,
-              // onChanged: (val) => titleInput = val でもおｋ
-              // onChanged: (val) {
-              //   titleInput = val;
-              // },
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: "Amount"),
               controller: amountController,
-              // onChanged: (val) {
-              //   amountInput = val;
-              // },
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text("Add ALL your time"),
               textColor: Colors.blue,
-              onPressed: () {
-                addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: () => submitData,
             )
           ],
         ),
