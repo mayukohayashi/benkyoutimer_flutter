5b27cd6af643dd72f72cfebba66c8b9d24a34fae
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2021-04-16 19:17:47 +0900

    update: 小数点以下1桁に

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -27,7 +27,7 @@ class TransactionList extends StatelessWidget {
                   )),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    "${transactions[index].amount.toStringAsFixed(2)} /Hour",
+                    "${transactions[index].amount.toStringAsFixed(1)} /Hour",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
