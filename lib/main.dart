9d37e67381730be71aa4a9f8e3b690ebb01b430c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2021-04-26 11:47:24 +0900

    add: height style

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,9 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction),
+            Expanded(
+              child: TransactionList(_userTransactions, _deleteTransaction),
+            ),
           ],
         ),
       ),
